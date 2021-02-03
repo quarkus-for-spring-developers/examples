fcabc5ec70d25c2b13cdfbece622991498f72ab9
Eric Deandrea <eric.deandrea@gmail.com>
2021-02-03 13:19:21 -0500

    Refactoring to use constructor injection

--- chapter-2/chapter-2-simple-project/src/main/java/org/acme/GreetingService.java
+++ chapter-2/chapter-2-simple-project/src/main/java/org/acme/GreetingService.java
@@ -6,8 +6,11 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 public class GreetingService {
-	@ConfigProperty(name = "greeting.name")
-	String greeting;
+	private final String greeting;
+
+	public GreetingService(@ConfigProperty(name = "greeting.name") String greeting) {
+		this.greeting = greeting;
+	}
 
 	public String getGreeting() {
 		return "Hello " + this.greeting;
