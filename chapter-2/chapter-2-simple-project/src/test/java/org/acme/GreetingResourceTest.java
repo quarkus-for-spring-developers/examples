38f85ea041f3903406b3fa10df5a5f03cd9f1205
Eric Deandrea <eric.deandrea@gmail.com>
2021-02-03 16:49:15 -0500

    Mocking in GreetingResourceTest

--- chapter-2/chapter-2-simple-project/src/test/java/org/acme/GreetingResourceTest.java
+++ chapter-2/chapter-2-simple-project/src/test/java/org/acme/GreetingResourceTest.java
@@ -1,21 +1,30 @@
 package org.acme;
 
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+
 @QuarkusTest
 public class GreetingResourceTest {
+	@InjectMock
+	GreetingService greetingService;
 
-    @Test
-    public void testHelloEndpoint() {
-        given()
-          .when().get("/hello-resteasy")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
-    }
+	@Test
+	public void testHelloEndpoint() {
+		Mockito.when(this.greetingService.getGreeting()).thenReturn("Hello Quarkus");
 
+		given()
+			.when().get("/hello-resteasy")
+			.then()
+				.statusCode(200)
+				.body(is("Hello Quarkus"));
+
+		Mockito.verify(this.greetingService).getGreeting();
+		Mockito.verifyNoMoreInteractions(this.greetingService);
+	}
 }
