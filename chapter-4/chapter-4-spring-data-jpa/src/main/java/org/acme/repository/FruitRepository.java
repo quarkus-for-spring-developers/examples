054670d9433c13f2b1ef2911c36537f210704e54
Eric Deandrea <eric.deandrea@gmail.com>
2021-04-06 14:35:38 -0400

    Removing @Repository annotation

--- chapter-4/chapter-4-spring-data-jpa/src/main/java/org/acme/repository/FruitRepository.java
+++ chapter-4/chapter-4-spring-data-jpa/src/main/java/org/acme/repository/FruitRepository.java
@@ -5,9 +5,7 @@ import java.util.Optional;
 import org.acme.domain.Fruit;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface FruitRepository extends JpaRepository<Fruit, Long> {
 	Optional<Fruit> findByName(String name);
 }
