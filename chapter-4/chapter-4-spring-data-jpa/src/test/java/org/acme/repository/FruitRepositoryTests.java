f0b599400ca051857fa050f564f42741b5f8582a
Eric Deandrea <eric.deandrea@gmail.com>
2021-04-08 16:56:38 -0400

    Change to use Spring Transactional annotation

--- chapter-4/chapter-4-spring-data-jpa/src/test/java/org/acme/repository/FruitRepositoryTests.java
+++ chapter-4/chapter-4-spring-data-jpa/src/test/java/org/acme/repository/FruitRepositoryTests.java
@@ -4,13 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Optional;
 
-import javax.transaction.Transactional;
-
 import org.acme.domain.Fruit;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Transactional
