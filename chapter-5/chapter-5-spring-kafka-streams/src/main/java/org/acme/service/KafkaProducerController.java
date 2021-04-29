105c88b500985fce22e705ef3e27567564e0de15
Eric Deandrea <eric.deandrea@gmail.com>
2021-04-29 16:34:07 -0400

    Chapter 5: spring-kafka-streams: KafkaProducerController send method uses both @GetMapping and @RequestMapping annotations
    Fixes #37

--- chapter-5/chapter-5-spring-kafka-streams/src/main/java/org/acme/service/KafkaProducerController.java
+++ chapter-5/chapter-5-spring-kafka-streams/src/main/java/org/acme/service/KafkaProducerController.java
@@ -1,8 +1,10 @@
 package org.acme.service;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,8 +16,8 @@ public class KafkaProducerController {
 		this.producer = producer;
 	}
 
-	@GetMapping
-	@RequestMapping("/{message}")
+	@GetMapping("/{message}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void send(@PathVariable String message) throws Exception {
 		this.producer.sendData(message);
 	}
