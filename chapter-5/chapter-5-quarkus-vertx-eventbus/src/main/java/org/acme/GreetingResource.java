b882bcc1d511d1873b72a640c5f78b0885b69adc
danieloh30 <doh@redhat.com>
2021-04-30 09:32:12 -0400

    Add @PathParam for consistency

--- chapter-5/chapter-5-quarkus-vertx-eventbus/src/main/java/org/acme/GreetingResource.java
+++ chapter-5/chapter-5-quarkus-vertx-eventbus/src/main/java/org/acme/GreetingResource.java
@@ -6,6 +6,7 @@ import io.vertx.mutiny.core.eventbus.Message;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -21,7 +22,7 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("{name}")
-    public Uni<String> greeting(String name) {
+    public Uni<String> greeting(@PathParam("name") String name) {
         return bus.<String>request("greeting", name)        
                  .onItem().transform(Message::body);
     }
