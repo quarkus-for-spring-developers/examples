4431c8176c43a1fa88b946a101bfc189b53b260c
danieloh30 <doh@redhat.com>
2021-05-07 23:33:03 -0400

    Add Tests

--- chapter-5/chapter-5-quarkus-vertx-eventbus/src/test/java/org/acme/GreetingResourceTest.java
+++ chapter-5/chapter-5-quarkus-vertx-eventbus/src/test/java/org/acme/GreetingResourceTest.java
@@ -1,21 +1,49 @@
 package org.acme;
 
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import io.quarkus.test.junit.QuarkusTest;
+import io.smallrye.mutiny.helpers.test.UniAssertSubscriber;
+
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.Test;
 
 @QuarkusTest
 public class GreetingResourceTest {
 
+    @Inject
+    GreetingResource greetingResource;
+
     @Test
-    public void testHelloEndpoint() {
-        given()
-          .when().get("/hello-resteasy")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+    public void testGreeting() {
+                
+        String message = this.greetingResource.greeting("quarkus events")
+        .subscribe()
+        .withSubscriber(UniAssertSubscriber.create())
+        .await()
+        .assertCompleted()
+        .getItem();
+
+        assertThat(message)
+            .isNotNull()
+            .isEqualTo("QUARKUS EVENTS");      
+
+    }
+
+    @Test
+    public void testBlockingConsumer() {
+                
+        String message = this.greetingResource.blockingConsumer("blocking events")
+        .subscribe()
+        .withSubscriber(UniAssertSubscriber.create())
+        .await()
+        .assertCompleted()
+        .getItem();
+
+        assertThat(message)
+            .isNotNull()
+            .isEqualTo("Processing Blocking I/O: blocking events");      
     }
 
 }
