4aa49523f69274b18bedcc813b5dfcbf83a27bcb
Daniel Oh <doh@redhat.com>
2021-05-12 08:15:44 -0400

    Add tests quarkus events (#47)
    Fixes #23

--- chapter-5/chapter-5-quarkus-vertx-eventbus/src/test/java/org/acme/GreetingResourceTest.java
+++ chapter-5/chapter-5-quarkus-vertx-eventbus/src/test/java/org/acme/GreetingResourceTest.java
@@ -1,21 +1,55 @@
 package org.acme;
 
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+import io.restassured.http.ContentType;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 @QuarkusTest
 public class GreetingResourceTest {
 
+    @InjectMock
+    GreetingService greetingService;
+
     @Test
-    public void testHelloEndpoint() {
+    public void testGreeting() {
+
+        Mockito.when(this.greetingService.consume(Mockito.eq("events")))
+            .thenReturn("EVENTS");
+            
         given()
-          .when().get("/hello-resteasy")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+            .when().get("/async/events")
+            .then()
+                .statusCode(200)
+                .contentType(ContentType.TEXT)
+                .body(is("EVENTS"));
+
+        Mockito.verify(this.greetingService).consume(Mockito.eq("events"));
+        Mockito.verifyNoMoreInteractions(this.greetingService);   
+
+    }
+
+    @Test
+    public void testBlockingConsumer() {
+
+        Mockito.when(this.greetingService.consumeBlocking(Mockito.eq("events")))
+            .thenReturn("Processing Blocking I/O: events");
+
+        given()
+            .when().get("/async/block/events")
+            .then()
+                .statusCode(200)
+                .contentType(ContentType.TEXT)
+                .body(is("Processing Blocking I/O: events"));
+
+        Mockito.verify(this.greetingService).consumeBlocking(Mockito.eq("events"));
+        Mockito.verifyNoMoreInteractions(this.greetingService);
+
     }
 
 }
