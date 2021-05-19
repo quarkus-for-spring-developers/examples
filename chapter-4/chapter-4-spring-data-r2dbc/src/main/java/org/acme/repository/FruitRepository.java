b76ef1e01dbf009f62b184daa72962c53d0ebf6c
Eric Deandrea <eric.deandrea@gmail.com>
2021-05-19 13:28:04 -0400

    Chapter 4: Spring Data R2DBC: Use ReactiveCrudRepository
    Fixes #54

--- chapter-4/chapter-4-spring-data-r2dbc/src/main/java/org/acme/repository/FruitRepository.java
+++ chapter-4/chapter-4-spring-data-r2dbc/src/main/java/org/acme/repository/FruitRepository.java
@@ -2,10 +2,10 @@ package org.acme.repository;
 
 import org.acme.domain.Fruit;
 
-import org.springframework.data.repository.reactive.ReactiveSortingRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import reactor.core.publisher.Mono;
 
-public interface FruitRepository extends ReactiveSortingRepository<Fruit, Long> {
+public interface FruitRepository extends ReactiveCrudRepository<Fruit, Long> {
 	Mono<Fruit> findByName(String name);
 }
