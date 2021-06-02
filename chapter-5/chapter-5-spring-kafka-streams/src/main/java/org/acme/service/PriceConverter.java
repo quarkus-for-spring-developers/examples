6a587a96c6e7ede39e1f3275cc382dd2e60d58b1
Daniel Oh <doh@redhat.com>
2021-06-02 16:24:31 -0400

    sync with text (#59)
    * sync with text
    
    * restore this

--- chapter-5/chapter-5-spring-kafka-streams/src/main/java/org/acme/service/PriceConverter.java
+++ chapter-5/chapter-5-spring-kafka-streams/src/main/java/org/acme/service/PriceConverter.java
@@ -2,16 +2,12 @@ package org.acme.service;
 
 import java.util.function.Consumer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import org.springframework.stereotype.Component;
 
 import reactor.core.publisher.Flux;
 
 @Component("priceconverter")
 public class PriceConverter implements Consumer<Flux<Integer>> {
-	private static final Logger LOGGER = LoggerFactory.getLogger(PriceConverter.class);
 	static final double CONVERSION_RATE = 0.88;
 
 	private final InMemoryChannel<Double> inMemoryChannel;
@@ -23,9 +19,7 @@ public class PriceConverter implements Consumer<Flux<Integer>> {
 	@Override
 	public void accept(Flux<Integer> priceInUsd) {
 		priceInUsd
-			.doOnNext(price -> LOGGER.debug("priceInUsd = {}", price))
 			.map(price -> price * CONVERSION_RATE)
-			.doOnNext(convertedPrice -> LOGGER.debug("convertedPrice = {}", convertedPrice))
 			.subscribe(this.inMemoryChannel::emitValue);
 	}
 }
