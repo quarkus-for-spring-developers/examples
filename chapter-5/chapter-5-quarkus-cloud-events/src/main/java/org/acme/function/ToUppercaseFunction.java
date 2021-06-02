bc9dde15deeefe8bc498dd0f3a20a4e34b5fa206
danieloh30 <doh@redhat.com>
2021-06-02 17:44:55 -0400

    Replace Logger with System.out

--- chapter-5/chapter-5-quarkus-cloud-events/src/main/java/org/acme/function/ToUppercaseFunction.java
+++ chapter-5/chapter-5-quarkus-cloud-events/src/main/java/org/acme/function/ToUppercaseFunction.java
@@ -2,6 +2,9 @@ package org.acme.function;
 
 import org.acme.domain.Input;
 import org.acme.domain.Output;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 import io.quarkus.funqy.Context;
 import io.quarkus.funqy.Funq;
@@ -9,16 +12,15 @@ import io.quarkus.funqy.knative.events.CloudEvent;
 
 public class ToUppercaseFunction {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ToUppercaseFunction.class);
+
     @Funq("uppercase")
     public Output function(Input input, @Context CloudEvent cloudEvent) {
         if (cloudEvent != null) {
-            System.out.println(
-                    "CloudEvent{" +
-                            "id='" + cloudEvent.id() + '\'' +
-                            ", specVersion='" + cloudEvent.specVersion() + '\'' +
-                            ", source='" + cloudEvent.source() + '\'' +
-                            ", subject='" + cloudEvent.subject() + '\'' +
-                            '}');
+            LOGGER.info("Input CE Id: {}", cloudEvent.id());
+            LOGGER.info("Input CE Spec Version: {}", cloudEvent.specVersion());
+            LOGGER.info("Input CE Source: {}", cloudEvent.source());
+            LOGGER.info("Input CE Subject: {}", cloudEvent.subject());
         }
         return new Output(input.getMessage().toUpperCase());
     }
