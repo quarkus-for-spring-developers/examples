3f09cf8b8f3ffba67df1e34343a37b90ac17f270
Eric Deandrea <eric.deandrea@gmail.com>
2021-06-11 09:30:28 -0400

    Use Map.of instead of Collections.singletonMap

--- chapter-5/chapter-5-quarkus-kafka-streams/src/test/java/org/acme/DockerComposeResource.java
+++ chapter-5/chapter-5-quarkus-kafka-streams/src/test/java/org/acme/DockerComposeResource.java
@@ -1,7 +1,6 @@
 package org.acme;
 
 import java.io.File;
-import java.util.Collections;
 import java.util.Map;
 
 import org.testcontainers.containers.DockerComposeContainer;
@@ -10,7 +9,6 @@ import org.testcontainers.containers.wait.strategy.Wait;
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
 
 public class DockerComposeResource implements QuarkusTestResourceLifecycleManager {
-
     static final DockerComposeContainer<?> DOCKER_COMPOSE =
 		new DockerComposeContainer<>(new File("docker-compose.yaml"))
 			.withExposedService("zookeeper", 1, 2181, Wait.forListeningPort())
@@ -19,7 +17,7 @@ public class DockerComposeResource implements QuarkusTestResourceLifecycleManage
     @Override
     public Map<String, String> start() {
         DOCKER_COMPOSE.start();
-        return Collections.singletonMap("kafka.bootstrap.servers", "localhost:9092");
+        return Map.of("kafka.bootstrap.servers", "localhost:9092");
     }
 
     @Override
