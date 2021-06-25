0dd84bc6f2ef8b28e6dd8a4b855511bf7a593770
Eric Deandrea <eric.deandrea@gmail.com>
2021-06-25 12:52:35 -0400

    Update timeout

--- chapter-5/chapter-5-spring-kafka-streams/src/test/java/org/acme/Chapter5SpringKafkaStreamsApplicationTests.java
+++ chapter-5/chapter-5-spring-kafka-streams/src/test/java/org/acme/Chapter5SpringKafkaStreamsApplicationTests.java
@@ -8,12 +8,14 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient(timeout = "PT45S")
 class Chapter5SpringKafkaStreamsApplicationTests extends DockerComposeBase {
 	@Autowired
 	WebTestClient webTestClient;
