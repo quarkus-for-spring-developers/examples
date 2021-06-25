cab5337c588642cd6fd21541f5316a59cd7bb5da
Eric Deandrea <eric.deandrea@gmail.com>
2021-06-25 13:10:36 -0400

    Adding DirtiesContext

--- chapter-4/chapter-4-spring-data-r2dbc/src/test/java/org/acme/TestContainerBase.java
+++ chapter-4/chapter-4-spring-data-r2dbc/src/test/java/org/acme/TestContainerBase.java
@@ -4,10 +4,12 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 
 @Testcontainers
+@DirtiesContext
 public abstract class TestContainerBase {
 	@Container
 	static final PostgreSQLContainer<?> DB = new PostgreSQLContainer<>("postgres:13")
@@ -18,9 +20,12 @@ public abstract class TestContainerBase {
 
 	@DynamicPropertySource
 	static void registerDynamicProperties(DynamicPropertyRegistry registry) {
-		registry.add("spring.r2dbc.url",
-			() -> String.format("r2dbc:postgresql://%s:%d/%s", DB.getHost(), DB.getFirstMappedPort(), DB.getDatabaseName()));
+		registry.add("spring.r2dbc.url", TestContainerBase::getDbUrl);
 		registry.add("spring.r2dbc.username", DB::getUsername);
 		registry.add("spring.r2dbc.password", DB::getPassword);
+	}
+
+	private static String getDbUrl() {
+		return String.format("r2dbc:postgresql://%s:%d/%s", DB.getHost(), DB.getFirstMappedPort(), DB.getDatabaseName());
 	}
 }
