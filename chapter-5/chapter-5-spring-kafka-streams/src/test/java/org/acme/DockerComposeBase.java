9b0be10647004b63c13dd7808ed6e2061eeeb447
Eric Deandrea <eric.deandrea@gmail.com>
2022-06-07 08:48:22 -0400

    Fix DockerComposeBase

--- chapter-5/chapter-5-spring-kafka-streams/src/test/java/org/acme/DockerComposeBase.java
+++ chapter-5/chapter-5-spring-kafka-streams/src/test/java/org/acme/DockerComposeBase.java
@@ -1,17 +1,37 @@
 package org.acme;
 
 import java.io.File;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 
-public class DockerComposeBase {
-	static final DockerComposeContainer<?> DOCKER_COMPOSE =
-		new DockerComposeContainer<>(new File("docker-compose.yaml"))
+public abstract class DockerComposeBase {
+	static final AtomicReference<DockerComposeContainer<?>> COMPOSE_CONTAINER_REFERENCE = new AtomicReference<>();
+
+	@BeforeAll
+	public static void beforeAll() {
+		COMPOSE_CONTAINER_REFERENCE
+			.updateAndGet(DockerComposeBase::createContainer)
+			.start();
+	}
+
+	@AfterAll
+	public static void afterAll() {
+		COMPOSE_CONTAINER_REFERENCE
+			.getAndSet(null)
+			.stop();
+	}
+
+	private static DockerComposeContainer<?> createContainer(DockerComposeContainer<?> existing) {
+		Optional.ofNullable(existing)
+			.ifPresent(DockerComposeContainer::stop);
+
+		return new DockerComposeContainer<>(new File("docker-compose.yaml"))
 			.withExposedService("zookeeper", 1, 2181, Wait.forListeningPort())
 			.withExposedService("kafka", 1, 9092, Wait.forListeningPort());
-
-	static {
-		DOCKER_COMPOSE.start();
 	}
 }
