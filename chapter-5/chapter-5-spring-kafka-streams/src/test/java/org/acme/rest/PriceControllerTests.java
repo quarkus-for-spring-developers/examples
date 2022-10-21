18aebd31979ebf57f3777e3b233df0594d8a32b1
Eric Deandrea <eric.deandrea@gmail.com>
2022-10-21 09:47:29 -0400

    Fixing Docker compose setup

--- chapter-5/chapter-5-spring-kafka-streams/src/test/java/org/acme/rest/PriceControllerTests.java
+++ chapter-5/chapter-5-spring-kafka-streams/src/test/java/org/acme/rest/PriceControllerTests.java
@@ -1,29 +1,51 @@
 package org.acme.rest;
 
+import java.io.File;
 import java.time.Duration;
 
-import org.acme.DockerComposeBase;
 import org.acme.service.InMemoryChannel;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import reactor.core.publisher.Flux;
 
 @SpringBootTest
 @AutoConfigureWebTestClient
-class PriceControllerTests extends DockerComposeBase {
+@Testcontainers
+class PriceControllerTests {
 	@Autowired
 	WebTestClient webTestClient;
 
 	@MockBean
 	InMemoryChannel<Double> inMemoryChannel;
+
+	@Container
+	static final DockerComposeContainer<?> DOCKER_COMPOSE =
+		new DockerComposeContainer<>(new File("docker-compose.yaml"))
+			.withExposedService("zookeeper", 1, 2181, Wait.forListeningPort())
+			.withExposedService("kafka", 1, 9092, Wait.forListeningPort());
+
+	@DynamicPropertySource
+	static void registerDynamicProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.cloud.stream.kafka.binder.brokers", PriceControllerTests::getKafkaBrokers);
+	}
+
+	private static String getKafkaBrokers() {
+		return String.format("%s:%s", DOCKER_COMPOSE.getServiceHost("kafka", 9092), DOCKER_COMPOSE.getServicePort("kafka", 9092));
+	}
 
 	@Test
 	public void stream() {
