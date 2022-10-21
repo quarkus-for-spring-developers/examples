58c63c2b9492dc1393084a9573b96f91c5d242bb
Eric Deandrea <eric.deandrea@gmail.com>
2022-10-21 13:14:28 -0400

    Use KafkaContainer

--- chapter-5/chapter-5-spring-kafka-streams/src/test/java/org/acme/Chapter5SpringKafkaStreamsApplicationTests.java
+++ chapter-5/chapter-5-spring-kafka-streams/src/test/java/org/acme/Chapter5SpringKafkaStreamsApplicationTests.java
@@ -2,46 +2,23 @@ package org.acme;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.File;
 import java.time.Duration;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.DockerComposeContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient(timeout = "PT10M")
-@Testcontainers
-class Chapter5SpringKafkaStreamsApplicationTests {
+class Chapter5SpringKafkaStreamsApplicationTests extends DockerComposeBase {
 	@Autowired
 	WebTestClient webTestClient;
-
-	@Container
-	static final DockerComposeContainer<?> DOCKER_COMPOSE =
-		new DockerComposeContainer<>(new File("docker-compose.yaml"))
-			.withExposedService("zookeeper", 1, 2181, Wait.forListeningPort())
-			.withExposedService("kafka", 1, 9092, Wait.forListeningPort());
-
-	@DynamicPropertySource
-	static void registerDynamicProperties(DynamicPropertyRegistry registry) {
-		registry.add("spring.cloud.stream.kafka.binder.brokers", Chapter5SpringKafkaStreamsApplicationTests::getKafkaBrokers);
-	}
-
-	private static String getKafkaBrokers() {
-		return String.format("%s:%s", DOCKER_COMPOSE.getServiceHost("kafka", 9092), DOCKER_COMPOSE.getServicePort("kafka", 9092));
-	}
 
 	@Test
 	void sseWorks() {
--- chapter-5/chapter-5-spring-kafka-streams/src/test/java/org/acme/DockerComposeBase.java
+++ chapter-5/chapter-5-spring-kafka-streams/src/test/java/org/acme/DockerComposeBase.java
@@ -1,11 +1,11 @@
 package org.acme;
 
-import java.io.File;
-
-import org.testcontainers.containers.DockerComposeContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 
@@ -14,19 +14,28 @@ import org.springframework.test.context.DynamicPropertySource;
  * test classes.
  * @see https://github.com/testcontainers/testcontainers-java/issues/5406
  */
+@Testcontainers
+@DirtiesContext
 public abstract class DockerComposeBase {
 	@Container
-	private static final DockerComposeContainer<?> DOCKER_COMPOSE =
-		new DockerComposeContainer<>(new File("docker-compose.yaml"))
-			.withExposedService("zookeeper", 1, 2181, Wait.forListeningPort())
-			.withExposedService("kafka", 1, 9092, Wait.forListeningPort());
+	static final KafkaContainer KAFKA =new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:6.2.1"));
 
 	@DynamicPropertySource
 	static void registerDynamicProperties(DynamicPropertyRegistry registry) {
-		registry.add("spring.cloud.stream.kafka.binder.brokers", DockerComposeBase::getKafkaBrokers);
+		registry.add("spring.cloud.stream.kafka.binder.brokers", KAFKA::getBootstrapServers);
 	}
-
-	private static String getKafkaBrokers() {
-		return String.format("%s:%s", DOCKER_COMPOSE.getServiceHost("kafka", 9092), DOCKER_COMPOSE.getServicePort("kafka", 9092));
-	}
+//	@Container
+//	private static final DockerComposeContainer<?> DOCKER_COMPOSE =
+//		new DockerComposeContainer<>(new File("docker-compose.yaml"))
+//			.withExposedService("zookeeper", 1, 2181, Wait.forListeningPort())
+//			.withExposedService("kafka", 1, 9092, Wait.forListeningPort());
+//
+//	@DynamicPropertySource
+//	static void registerDynamicProperties(DynamicPropertyRegistry registry) {
+//		registry.add("spring.cloud.stream.kafka.binder.brokers", DockerComposeBase::getKafkaBrokers);
+//	}
+//
+//	private static String getKafkaBrokers() {
+//		return String.format("%s:%s", DOCKER_COMPOSE.getServiceHost("kafka", 9092), DOCKER_COMPOSE.getServicePort("kafka", 9092));
+//	}
 }
