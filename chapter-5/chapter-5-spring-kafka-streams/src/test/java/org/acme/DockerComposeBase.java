b51f2780688f86cde3151bb9b34bdc18184454e3
Eric Deandrea <eric.deandrea@gmail.com>
2022-10-21 09:38:29 -0400

    Revert "Fixing Docker compose setup"
    This reverts commit a79795fe363166af46541dc13e74a1eac8b463bc.

--- chapter-5/chapter-5-spring-kafka-streams/src/test/java/org/acme/DockerComposeBase.java
+++ chapter-5/chapter-5-spring-kafka-streams/src/test/java/org/acme/DockerComposeBase.java
@@ -1,21 +1,37 @@
 package org.acme;
 
 import java.io.File;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 
 public abstract class DockerComposeBase {
-	private static final DockerComposeContainer<?> DOCKER_COMPOSE = new DockerComposeContainer<>(new File("docker-compose.yaml"))
-			.withExposedService("zookeeper", 1, 2181, Wait.forListeningPort())
-			.withExposedService("kafka", 1, 9092, Wait.forListeningPort());
+	static final AtomicReference<DockerComposeContainer<?>> COMPOSE_CONTAINER_REFERENCE = new AtomicReference<>();
 
-	static {
-		DOCKER_COMPOSE.start();
-		System.getProperties().setProperty("spring.cloud.stream.kafka.binder.brokers", getKafkaBrokers());
+	@BeforeAll
+	public static void beforeAll() {
+		COMPOSE_CONTAINER_REFERENCE
+			.updateAndGet(DockerComposeBase::createContainer)
+			.start();
 	}
 
-	private static String getKafkaBrokers() {
-		return String.format("%s:%s", DOCKER_COMPOSE.getServiceHost("kafka", 9092), DOCKER_COMPOSE.getServicePort("kafka", 9092));
+	@AfterAll
+	public static void afterAll() {
+		COMPOSE_CONTAINER_REFERENCE
+			.getAndSet(null)
+			.stop();
+	}
+
+	private static DockerComposeContainer<?> createContainer(DockerComposeContainer<?> existing) {
+		Optional.ofNullable(existing)
+			.ifPresent(DockerComposeContainer::stop);
+
+		return new DockerComposeContainer<>(new File("docker-compose.yaml"))
+			.withExposedService("zookeeper", 1, 2181, Wait.forListeningPort())
+			.withExposedService("kafka", 1, 9092, Wait.forListeningPort());
 	}
 }
